78c17dc877bca6a88a0584b5dd92853cf921bff6
levanthoi <lethoi1506@gmail.com>
2023-06-13 21:55:25 +0700

    update

--- Farm_Management/Khachhang.cs
+++ Farm_Management/Khachhang.cs
@@ -16,27 +16,5 @@ namespace Farm_Management
         {
             InitializeComponent();
         }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Khachhang_Load(object sender, EventArgs e)
-        {
-            // TODO: This line of code loads data into the 'quanLyTrangTraiDataSet1.KhachHang' table. You can move, or remove it, as needed.
-            this.khachHangTableAdapter1.Fill(this.quanLyTrangTraiDataSet1.KhachHang);
-            // TODO: This line of code loads data into the 'quanLyTrangTraiDataSet.KhachHang' table. You can move, or remove it, as needed.
-            this.khachHangTableAdapter.Fill(this.quanLyTrangTraiDataSet.KhachHang);
-            BindingSource bs = new BindingSource();
-            dgv_khachhang.DataSource = bs;
-            bdn_KH.BindingSource = bs;
-
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-
-        }
     }
 }
